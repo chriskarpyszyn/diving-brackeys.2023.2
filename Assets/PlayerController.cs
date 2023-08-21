a210be141518c8cd42c1275ef58e4c9739910fb4
Chris Karpyszyn <ckarpyszyn@uncommonpurpose.com>
2023-08-20 20:25:51 -0400

    horizontal movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -14,6 +14,9 @@ public class PlayerController : MonoBehaviour
     public float acceleration = 0.1f;
     public float maxFallSpeed = 2f;
 
+    //movement 
+    public float moveSpeed = 8f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +26,16 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //falling movement
         fallSpeed = Mathf.Min(fallSpeed + acceleration * Time.deltaTime, maxFallSpeed);
         transform.position -= new Vector3(0, fallSpeed * Time.deltaTime, 0);
+
+        //player left/right movement
+        float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(horizontal, 0, 0);
+        moveDirection.Normalize();
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
     }
 }
