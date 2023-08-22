f96f3a7ea6615ef48738bfbf4e03fac2d367c830
Chris Karpyszyn <ckarpyszyn@uncommonpurpose.com>
2023-08-21 20:56:57 -0400

    fix auto generation of enemies

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //scores and other ui data
     private int scoreDepth = 0;
-
     private int playerHealth = 3;
+
+    //vars for enemy generation
+    public GameObject enemyPrefab;
+    public List<GameObject> enemies;
+    public int enemyDivider = 5;
 
     void Start()
     {
         Application.targetFrameRate = 60;
+        
+        //TODO: I can make this better.... kind of janky.
+        createEnemies(0, 2);
     }
 
     void Update()
@@ -36,5 +44,24 @@ public class GameManager : MonoBehaviour
     public int GetHealth()
     {
         return playerHealth;
+    }
+
+    public void createEnemies(float yPos, float zoneOffset)
+    {
+        int depth = scoreDepth;
+        //int randomNumber = Random.Range(1, 5); //TODO: I'd like things to be a bit more random...
+        int randomNumber = 0;
+        int numberOfEnemiesToGenerate = (depth / enemyDivider) + randomNumber;
+        float doubleZoneOffset = zoneOffset * 2; //no magic numbers... kinda
+        float randomXRange = 9f;
+
+        for (int i = 0; i <= numberOfEnemiesToGenerate; i++)
+        {
+            float randomX = Random.Range(-randomXRange, randomXRange);
+            //int randomYIntRange = Random.Range(-20, 21);
+            //float randomY = randomYIntRange * 0.5f;
+            float randomY = Random.Range(yPos - doubleZoneOffset + 0.5f, yPos - -zoneOffset - doubleZoneOffset - 0.5f);
+            Instantiate(enemyPrefab, new Vector3(randomX, randomY, enemyPrefab.transform.position.z), Quaternion.identity);
+        }
     }
 }
--- Assets/Scripts/GenerateMapOnDetection.cs
+++ Assets/Scripts/GenerateMapOnDetection.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GenerateMapOnDetection : MonoBehaviour
@@ -8,33 +9,21 @@ public class GenerateMapOnDetection : MonoBehaviour
 
     public GameObject enemy;
 
+    //TODO: I could move this to the game manager.
     private int zoneOffset = 10;
-    public int enemyDivider = 5;
+    
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            Debug.Log("Hi there");
             GameManager gm = FindObjectOfType<GameManager>();
 
             Transform gameObjectTransform = gameObject.transform;
             Instantiate(gameObject, new Vector3(gameObjectTransform.position.x, gameObjectTransform.position.y - zoneOffset, gameObjectTransform.position.z), Quaternion.identity);
 
-
-            
-            int depth = gm.GetDepth();
-            int randomNumber = Random.Range(1, 4);
-            int numberOfEnemiesToGenerate = (depth / enemyDivider) + randomNumber;
-
-            for (int i = 0; i <= numberOfEnemiesToGenerate; i++)
-            {
-                float randomX = Random.Range(-9.99f, 9.99f);
-                float randomY = Random.Range(gameObjectTransform.position.y + 0.01f, gameObjectTransform.position.y - zoneOffset - 0.01f);
-                Instantiate(enemy, new Vector3(randomX, randomY, enemy.transform.position.z), Quaternion.identity);
-            }
-
+            gm.createEnemies(gameObjectTransform.position.y, zoneOffset);
 
             //destroy this gameobject
             Destroy(gameObject);
